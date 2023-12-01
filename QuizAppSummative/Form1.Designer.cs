347bc5ce878eb280dfa740c69b24ce3391877913
David Ntwari <d.ntwari@alustudent.com>
2023-12-01 14:34:47 +0200

    Add project files.

---
+++ QuizAppSummative/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace QuizAppSummative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.quizQuestion = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // quizQuestion
+            // 
+            this.quizQuestion.Font = new System.Drawing.Font("Lucida Handwriting", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quizQuestion.Location = new System.Drawing.Point(86, 405);
+            this.quizQuestion.Name = "quizQuestion";
+            this.quizQuestion.Size = new System.Drawing.Size(950, 95);
+            this.quizQuestion.TabIndex = 1;
+            this.quizQuestion.Text = "Pick a Topic";
+            this.quizQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 525);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(318, 60);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 606);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(318, 60);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.checkAnswer);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(436, 525);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(318, 60);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.checkAnswer);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(436, 606);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(318, 60);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.checkAnswer);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(838, 525);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(318, 60);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.checkAnswer);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(838, 606);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(318, 60);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.checkAnswer);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::QuizAppSummative.Properties.Resources.quiz_time;
+            this.pictureBox1.Location = new System.Drawing.Point(92, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1024, 390);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1188, 692);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.quizQuestion);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.checkAnswer);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label quizQuestion;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ QuizAppSummative/Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace QuizAppSummative
+{
+    /// <summary>
+    /// Represents the main form of the quiz application.
+    /// </summary>
+    public partial class Form1 : Form
+{
+    // necessary variables
+    private int correctAnswer;
+    private int questionNumber = 0;
+    private int score;
+    private int totalQuestions;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Form1"/> class.
+    /// </summary>
+    public Form1()
+    {
+        InitializeComponent();
+
+        askQuestions(questionNumber);
+        totalQuestions = 8;
+    }
+
+    /// <summary>
+    /// Handles the click event of the first button.
+    /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+    private void button1_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            if (questionNumber == 0)
+            {
+                questionNumber++;
+                askQuestions(questionNumber);
+            }
+        }
+        catch (Exception ex)
+        {
+            HandleException(ex);
+        }
+    }
+
+    /// <summary> /// Handles the click event of the answer buttons.
+    /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+    private void checkAnswer(object sender, EventArgs e)
+    {
+        try
+        {
+            if (sender is Button senderButton) // Check if the sender is a Button
+            {
+                int buttonTag = Convert.ToInt32(senderButton.Tag);
+
+                Console.WriteLine($"Button {buttonTag} clicked for question {questionNumber}");
+
+                string userAnswer = "";
+
+                if (questionNumber > 0)
+                {
+                    // Get the text of the selected answer
+                    userAnswer = senderButton.Text;
+
+                    if (buttonTag == correctAnswer)
+                    {
+                        score++;
+                        // Provide immediate feedback
+                        MessageBox.Show($"Correct! The correct answer is {userAnswer}.");
+                    }
+                    else
+                    {
+                        // Get the name of the correct answer
+                        string correctAnswerText = askQuestions(questionNumber);
+                        // Provide immediate feedback
+                        MessageBox.Show($"Wrong! The correct answer is {correctAnswerText}. You selected {userAnswer}.");
+                    }
+                }
+
+                if (questionNumber == totalQuestions - 1)
+                {
+                    // work out the percentage
+                    int percentage = (int)Math.Round((double)(score * 100) / totalQuestions);
+                    MessageBox.Show(
+                        "Quiz Ended!" + Environment.NewLine +
+                        "You have answered " + score + " questions correctly." + Environment.NewLine +
+                        "Your total percentage is " + percentage + "%" + Environment.NewLine +
+                        "Click OK to play again"
+                    );
+                    score = 0;
+                    questionNumber = 0;
+                    askQuestions(questionNumber);
+                }
+                else
+                {
+                    questionNumber++;
+                    askQuestions(questionNumber);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            HandleException(ex);
+        }
+    }
+
+    /// <summary>
+    /// Helper method to get the text of the answer based on the option number.
+    /// </summary>
+    /// <param name="questNum">The question number.</param>
+    /// <returns>The text of the answer.</returns>
+    private string askQuestions(int questNum)
+    {
+        try
+        {
+            switch (questNum)
+            {
+                case 0:
+                    pictureBox1.Image = Properties.Resources.quiz_time;
+                    quizQuestion.Text = "Pick a Topic";
+                    button1.Text = "Movies";
+                    button2.Text = "Science";
+                    button3.Text = "Animals";
+                    button4.Text = "Technology";
+                    button5.Text = "Cars";
+                    button6.Text = "Countries";
+
+                    // Use Tag property to set correct answer
+                    button1.Tag = 1;
+                    button2.Tag = 2;
+                    button3.Tag = 3;
+                    button4.Tag = 4;
+                    button5.Tag = 5;
+                    button6.Tag = 6;
+
+                    correctAnswer = 1;
+                    return "Movies";
+
+                    case 1:
+                        pictureBox1.Image = Properties.Resources.quentin_tarantino;
+                        quizQuestion.Text = "Who directed the 1994 classic 'Pulp Fiction'?";
+                        button1.Text = "Martin Scorsese";
+                        button2.Text = "David Fincher";
+                        button3.Text = "David Lynch";
+                        button4.Text = "Quentin Tarantino";
+                        button5.Text = "Wes Anderson";
+                        button6.Text = "Christopher Nolan";
+
+                        // Use Tag property to set correct answer
+                        button1.Tag = 1;
+                        button2.Tag = 2;
+                        button3.Tag = 3;
+                        button4.Tag = 4;
+                        button5.Tag = 5;
+                        button6.Tag = 6;
+
+                        correctAnswer = 4;
+                        return "Quentin Tarantino";
+
+                    case 2:
+                        pictureBox1.Image = Properties.Resources.Japan;
+                        quizQuestion.Text = "Where is the movie 'Bullet Train' set?";
+                        button1.Text = "Italy";
+                        button2.Text = "Japan";
+                        button3.Text = "Prague";
+                        button4.Text = "France";
+                        button5.Text = "Greece";
+                        button6.Text = "Costa Rica";
+
+                        // Use Tag property to set correct answer
+                        button1.Tag = 1;
+                        button2.Tag = 2;
+                        button3.Tag = 3;
+                        button4.Tag = 4;
+                        button5.Tag = 5;
+                        button6.Tag = 6;
+
+                        correctAnswer = 2;
+                        return "Japan";
+
+                    case 3:
+                        pictureBox1.Image = Properties.Resources.Clarice_Starling;
+                        quizQuestion.Text = "What is the name of the female lead in the thriller 'Silence of the lamb'?";
+                        button1.Text = "Ardelia Mapp";
+                        button2.Text = "Marcy Madsen";
+                        button3.Text = "Clarice Starling";
+                        button4.Text = "Miriam Lass";
+                        button5.Text = "Judy Hopps";
+                        button6.Text = "Catherine Martin";
+
+                        // Use Tag property to set correct answer
+                        button1.Tag = 1;
+                        button2.Tag = 2;
+                        button3.Tag = 3;
+                        button4.Tag = 4;
+                        button5.Tag = 5;
+                        button6.Tag = 6;
+
+                        correctAnswer = 3;
+                        return "Clarice Starling";
+
+                    case 4:
+                        pictureBox1.Image = Properties.Resources.pierce_brosnan;
+                        quizQuestion.Text = "What is the name of this James Bond?";
+                        button1.Text = "Timothy Dalton";
+                        button2.Text = "Roger Moore";
+                        button3.Text = "Matthew McCognauhey";
+                        button4.Text = "Sean Connery";
+                        button5.Text = "Daniel Craig";
+                        button6.Text = "Pierce Brosnan";
+
+                        // Use Tag property to set correct answer
+                        button1.Tag = 1;
+                        button2.Tag = 2;
+                        button3.Tag = 3;
+                        button4.Tag = 4;
+                        button5.Tag = 5;
+                        button6.Tag = 6;
+
+                        correctAnswer = 6;
+                        return "Pierce Brosnan";
+
+                    case 5:
+                        pictureBox1.Image = Properties.Resources.heath_ledger;
+                        quizQuestion.Text = "Who portrayed Joker in the 2008 classic 'Dark Knight'?";
+                        button1.Text = "Jared Leto";
+                        button2.Text = "Jack Nicholson";
+                        button3.Text = "Joaquin Phoenix";
+                        button4.Text = "Mark Hamil";
+                        button5.Text = "Heath Ledger";
+                        button6.Text = "Robert Downey Jr";
+
+                        // Use Tag property to set correct answer
+                        button1.Tag = 1;
+                        button2.Tag = 2;
+                        button3.Tag = 3;
+                        button4.Tag = 4;
+                        button5.Tag = 5;
+                        button6.Tag = 6;
+
+                        correctAnswer = 5;
+                        return "Heath Ledger";
+
+                    case 6:
+                        pictureBox1.Image = Properties.Resources.martin_scorcesse;
+                        quizQuestion.Text = "Who directed the iconic mob thriller 'GoodFellas'?";
+                        button1.Text = "Francis Ford Capolla";
+                        button2.Text = "Martin Scorcesese";
+                        button3.Text = "Quentin Tarantino";
+                        button4.Text = "Steven Spielberg";
+                        button5.Text = "Christopher Nolan";
+                        button6.Text = "Brian De Palma";
+
+                        // Use Tag property to set correct answer
+                        button1.Tag = 1;
+                        button2.Tag = 2;
+                        button3.Tag = 3;
+                        button4.Tag = 4;
+                        button5.Tag = 5;
+                        button6.Tag = 6;
+
+                        correctAnswer = 2;
+                        return "Martin Scorcesese";
+
+                    case 7:
+                        pictureBox1.Image = Properties.Resources.you_do_not_talk_about_fight_club;
+                        quizQuestion.Text = "What movie is this picture from?";
+                        button1.Text = "Inside Out";
+                        button2.Text = "GodFather";
+                        button3.Text = "Gone Girl";
+                        button4.Text = "Hoops and Boobs";
+                        button5.Text = "Training Day";
+                        button6.Text = "Fight Club";
+
+                        // Use Tag property to set correct answer
+                        button1.Tag = 1;
+                        button2.Tag = 2;
+                        button3.Tag = 3;
+                        button4.Tag = 4;
+                        button5.Tag = 5;
+                        button6.Tag = 6;
+
+                        correctAnswer = 6;
+                        return "Fight Club";
+
+
+
+                    default:
+                        return ""; // Handle default case appropriately
+                }
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+                return ""; // Handle the case when an exception occurs in askQuestions
+            }
+        }
+
+        /// <summary>
+        /// Handles exceptions by showing an error message.
+        /// </summary>
+        /// <param name="ex">The exception.</param>
+        private void HandleException(Exception ex)
+        {
+            MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
